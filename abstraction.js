3c04c45dd9ae2ac6121eff4d42b963104bbdc580
root <root@localhost.localdomain>
2023-07-23 11:48:59 +0530

    oops

--- abstraction.js
+++ abstraction.js
@@ -1,9 +1,14 @@
 class chiefBuilding{
     drugProportion(){
         data =  'confidential drug proportion'
+        return data
     }
+    abc(){}
 }
 class outletBuilding extends chiefBuilding{
+    // abc(){
+    //     // functionality
+    // }
     painKiller(){
         let drugProportiona = this.drugProportion()
         let tablet = drugProportiona + "mfg process"
@@ -13,3 +18,14 @@ class outletBuilding extends chiefBuilding{
 
 outletBuildingObj = new outletBuilding()
 console.log(outletBuildingObj.painKiller())
+
+
+class shop{
+    counter(){}
+    groceryCorner(){}
+    stationeryCorner(){}
+}
+
+class ravalnathShop{
+
+}
